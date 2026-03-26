68278fd5889f2ef7654e845a760839b4a34a5135
Sean Boots <sean.boots@gmail.com>
2026-03-25 22:43:00 -0700

    Optionally save or view the run log after uploading.

--- bulk_upload.R
+++ bulk_upload.R
@@ -10,3 +10,7 @@ source("lib/ckan_helpers.R")
 
 # upload_all_files_to_package("non-existent-bulk-upload-testing-001")
 
+# Optionally list the run log or export it to CSV:
+
+# run_log |> write_csv("log/run_log.csv")
+# run_log |> View()
