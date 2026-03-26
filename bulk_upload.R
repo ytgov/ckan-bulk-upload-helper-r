d79878012b9de4d608ebeb338b1fa0ff9354d4dd
Sean Boots <sean.boots@gmail.com>
2026-03-26 10:25:21 -0700

    Updated examples

--- bulk_upload.R
+++ bulk_upload.R
@@ -2,15 +2,20 @@
 source("lib/ckan_helpers.R")
 
 # Example usage:
-# This uploads all the files in "input/bulk-upload-testing-001" to the package named "bulk-upload-testing-001".
+
+# 1. Upload all (new) resource files in input/*, using a directory for each package that matches the CKAN package name.
+
+# upload_all_input_files()
+
+# 2a. Alternatively, upload a specific directory. This adds any new files in "input/bulk-upload-testing-001" to the package named "bulk-upload-testing-001".
 
 # upload_all_files_to_package("bulk-upload-testing-001")
 
-# If the package does not exist on the CKAN instance specified in .env, it will raise an error message.
+# 2b. If the package does not exist on the CKAN instance specified in .env, it will raise an error message.
 
 # upload_all_files_to_package("non-existent-bulk-upload-testing-001")
 
-# Optionally list the run log or export it to CSV:
+# 3. After finishing, optionally list the run log or export it to CSV:
 
 # run_log |> write_csv("log/run_log.csv")
 # run_log |> View()
