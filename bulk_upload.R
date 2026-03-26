7653cbe6340040a6d5b6058add83b111017e1b38
Sean Boots <sean.boots@gmail.com>
2026-03-25 22:39:42 -0700

    Example function usage.

--- bulk_upload.R
+++ bulk_upload.R
@@ -1,3 +1,12 @@
 # Load tidyverse libraries, and set up CKAN URLs and API tokens
 source("lib/ckan_helpers.R")
 
+# Example usage:
+# This uploads all the files in "input/bulk-upload-testing-001" to the package named "bulk-upload-testing-001".
+
+# upload_all_files_to_package("bulk-upload-testing-001")
+
+# If the package does not exist on the CKAN instance specified in .env, it will raise an error message.
+
+# upload_all_files_to_package("non-existent-bulk-upload-testing-001")
+
